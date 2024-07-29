4f2cb9c2cb130167ca62af51c18d64f0fdc1e296
joii2020 <joii@cryptape.com>
2024-07-29 17:06:31 +0800

    Fix bug on unit test

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -4,8 +4,8 @@
 use ckb_testtool::{
     ckb_types::{
         bytes::Bytes,
-        core::{ScriptHashType, TransactionBuilder},
-        packed::{CellInput, CellOutput},
+        core::{DepType, ScriptHashType, TransactionBuilder},
+        packed::{CellDep, CellInput, CellOutput},
         prelude::*,
     },
     context::Context,
@@ -19,6 +19,13 @@ fn test_exec() {
     let out_point_exec_parent = context.deploy_cell_by_name("exec-parent");
     let out_point_exec_child = context.deploy_cell_by_name("exec-child");
 
+    let exec_child_code_hash = context
+        .cells
+        .get(&out_point_exec_child)
+        .map(|(_, bin)| CellOutput::calc_data_hash(bin).as_bytes().to_vec())
+        .unwrap();
+    println!("=== exec child code hash: {:02x?}", &exec_child_code_hash);
+
     let lock_script = context
         .build_script_with_hash_type(
             &out_point_exec_parent,
@@ -28,16 +35,9 @@ fn test_exec() {
         .expect("script")
         .as_builder()
         .args(
-            vec![
-                context
-                    .cells
-                    .get(&out_point_exec_child)
-                    .map(|(_, bin)| CellOutput::calc_data_hash(bin).as_bytes().to_vec())
-                    .unwrap(),
-                vec![ScriptHashType::Data2.into()],
-            ]
-            .concat()
-            .pack(),
+            vec![exec_child_code_hash, vec![ScriptHashType::Data2.into()]]
+                .concat()
+                .pack(),
         )
         .build();
     let input: CellInput = CellInput::new_builder()
@@ -71,7 +71,14 @@ fn test_exec() {
         .set_inputs(vec![input])
         .outputs(outputs)
         .outputs_data(outputs_data.pack())
+        .cell_dep(
+            CellDep::new_builder()
+                .out_point(out_point_exec_child)
+                .dep_type(DepType::Code.into())
+                .build(),
+        )
         .build();
+
     let tx = context.complete_tx(tx);
 
     // run
@@ -81,4 +88,71 @@ fn test_exec() {
 }
 
 #[test]
-fn test_spanw() {}
+fn test_spanw() {
+    let mut context = Context::default();
+    let out_point_parent = context.deploy_cell_by_name("spawn-parent");
+    let out_point_child = context.deploy_cell_by_name("spawn-child");
+
+    let exec_child_code_hash = context
+        .cells
+        .get(&out_point_child)
+        .map(|(_, bin)| CellOutput::calc_data_hash(bin).as_bytes().to_vec())
+        .unwrap();
+    println!("=== exec child code hash: {:02x?}", &exec_child_code_hash);
+
+    let lock_script = context
+        .build_script_with_hash_type(&out_point_parent, ScriptHashType::Data2, Default::default())
+        .expect("script")
+        .as_builder()
+        .args(
+            vec![exec_child_code_hash, vec![ScriptHashType::Data2.into()]]
+                .concat()
+                .pack(),
+        )
+        .build();
+    let input: CellInput = CellInput::new_builder()
+        .previous_output(
+            context.create_cell(
+                CellOutput::new_builder()
+                    .capacity(1000u64.pack())
+                    .lock(lock_script.clone())
+                    .build(),
+                Bytes::new(),
+            ),
+        )
+        .build();
+
+    let outputs = vec![
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script.clone())
+            .build(),
+        CellOutput::new_builder()
+            .capacity(500u64.pack())
+            .lock(lock_script)
+            .build(),
+    ];
+
+    let outputs_data = vec![Bytes::new(); 2];
+
+    // build transaction
+    let tx = TransactionBuilder::default()
+        // .set_inputs(vec![input, input3, input2])
+        .set_inputs(vec![input])
+        .outputs(outputs)
+        .outputs_data(outputs_data.pack())
+        .cell_dep(
+            CellDep::new_builder()
+                .out_point(out_point_child)
+                .dep_type(DepType::Code.into())
+                .build(),
+        )
+        .build();
+
+    let tx = context.complete_tx(tx);
+
+    // run
+    context
+        .verify_tx(&tx, MAX_CYCLES)
+        .expect("pass verification");
+}
